3e1733a86fa7c25c401ef7b3cb7134699ac6fa21
Joshua M. Boniface <joshua@boniface.me>
2020-07-27 21:11:20 -0400

    Fix checking for Docker versions

--- collect-server.azure.sh
+++ collect-server.azure.sh
@@ -377,8 +377,10 @@ do_docker_meta() {
     if [[ -z ${is_unstable} ]]; then
         server_ok="n"
         web_ok="n"
-        curl --silent -f -lSL https://index.docker.io/v1/repositories/jellyfin/jellyfin-server/tags/${version} >/dev/null && server_ok="y"
-        curl --silent -f -lSL https://index.docker.io/v1/repositories/jellyfin/jellyfin-web/tags/${version} >/dev/null && web_ok="y"
+        for arch in ${docker_arches[@]}; do
+            curl --silent -f -lSL https://index.docker.io/v1/repositories/jellyfin/jellyfin-server/tags/stable-${build_id}-${arch} >/dev/null && server_ok="y" || server_ok="n"
+        done
+        curl --silent -f -lSL https://index.docker.io/v1/repositories/jellyfin/jellyfin-web/tags/stable-${build_id} >/dev/null && web_ok="y" || web_ok="n"
         if [[ ${server_ok} != "y" || ${web_ok} != "y" ]]; then
             return
         fi
