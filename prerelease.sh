a1ee4268adcc0ecf4d6918206f6b4087a74f3cd9
Joshua Boniface <joshua@boniface.me>
2022-03-29 02:00:31 -0400

    Fix metapackage versioning

--- prerelease.sh
+++ prerelease.sh
@@ -303,6 +303,7 @@ do_deb_meta() {
 
     releasedir="versions/stable-pre/meta/${version}"
     linkdir="stable-pre"
+    version="$( sed 's/-/~/g' <<<"${version}" )"
     versend=""
 
     sed -i "s/X.Y.Z/${version}${versend}/g" jellyfin.debian
